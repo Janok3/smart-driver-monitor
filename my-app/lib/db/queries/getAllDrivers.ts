174690b41d885a66934ea9a1485ebd843823847e
Michael <cold.bubbling.tea@gmail.com>
2025-04-09 12:29:18 +0800

    refactor: Return drivers in order

--- my-app/lib/db/queries/getAllDrivers.ts
+++ my-app/lib/db/queries/getAllDrivers.ts
@@ -4,7 +4,7 @@ import { Driver } from "@/lib/types";
 
 export default async function getAllDrivers(): Promise<Driver[]> {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM DRIVERS');
+    const result = await client.query('SELECT * FROM DRIVERS ORDER BY driver_id ASC');
     client.release();   
 
     return result.rows;
